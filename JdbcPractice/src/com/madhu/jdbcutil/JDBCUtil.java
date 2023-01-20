1a60d269effdf8e0f09df4aa6505bd08c8a93cd1
madhu <madhu.java.git@gmail.com>
2023-01-20 23:04:06 +0530

    coding all common statemts in one util class

---
+++ JdbcPractice/src/com/madhu/jdbcutil/JDBCUtil.java
@@ -0,0 +1,43 @@
+package com.madhu.jdbcutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.x.protobuf.MysqlxConnection.Close;
+
+public class JDBCUtil {
+private JDBCUtil(){}
+static {
+	//step 1 : loading and registering the driver
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+}
+
+public static Connection getJdbcConnection() throws SQLException {
+	String url ="jdbc:mysql:///student";
+	String username="root";
+	String password ="root";
+	Connection connection = DriverManager.getConnection(url,username,password);
+	System.out.println("connection object created...");
+	return connection;
+}
+
+public static  void closeResources(Connection connection, Statement statement,ResultSet resultSet) throws SQLException {
+	if(connection!=null) {
+		connection.close();
+	}
+	if(statement!=null) {
+		statement.close();
+	}
+	if(resultSet!=null) {
+		resultSet.close();
+	}
+}
+
+}
