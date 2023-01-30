a5ca4f78afebf78b7d3b1ec6a7b08911e2810f14
madhu <madhu.java.git@gmail.com>
2023-01-31 03:37:16 +0530

    prepared st with dynamic input

--- JdbcPractice/src/com/madhu/preparedst/PreparedStEx.java
+++ JdbcPractice/src/com/madhu/preparedst/PreparedStEx.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import com.madhu.jdbcutil.JDBCUtil;
 
@@ -23,11 +24,32 @@ try {
 		preparedStatement=connection.prepareStatement(sqlInsertQuery);
     if(preparedStatement!=null) {
     	//use precompiled query to set the values
-    	preparedStatement.setInt(1, 101);
+    	/*preparedStatement.setInt(1, 101);
     	preparedStatement.setString(2, "AAA");
     	preparedStatement.setInt(3, 45);
     	preparedStatement.setString(4, "MI");
     	System.out.println(sqlInsertQuery);
+    	*/
+    	
+    	//take the dynamic input
+    	Scanner scanner= new Scanner(System.in);
+    	System.out.println("Enter the id");
+    	int sid = scanner.nextInt();
+    	
+    	System.out.println("Enter the name");
+    	String sname = scanner.next();
+    	
+    	System.out.println("Enter the age");
+    	int sage = scanner.nextInt();
+    	
+    	System.out.println("Enter the city");
+    	String scity = scanner.next();
+    	
+    	
+    	preparedStatement.setInt(1, sid);
+    	preparedStatement.setString(2, sname);
+    	preparedStatement.setInt(3, sage);
+    	preparedStatement.setString(4, scity);
     	//execute the query
     	int rowsAffeted=preparedStatement.executeUpdate();
     }
